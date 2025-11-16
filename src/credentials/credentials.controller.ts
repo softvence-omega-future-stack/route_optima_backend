2f0b155f90b541be87cbae717b17c47a1bf4a378
rockyhaque <rockyhaque@gmail.com>
2025-11-16 11:48:59 +0600

    fix: auth gurad issues

--- src/credentials/credentials.controller.ts
+++ src/credentials/credentials.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res, UseGuards } from '@nestjs/common';
 import { CredentialsService } from './credentials.service';
 import { Response } from 'express';
+import { AuthGuard } from 'src/auth/guards/jwt-auth-guard';
+import { RolesGuard } from 'src/auth/guards/role-guard';
+import { AuthRoles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from '@prisma/client';
 
 @Controller('api/v1/credentials')
 export class CredentialsController {
   constructor(private readonly credentialsService: CredentialsService) {}
 
   @Get()
+  @UseGuards(AuthGuard, RolesGuard)
+  @AuthRoles(UserRole.ADMIN)
   async getCredentials(@Res() res: Response) {
     const result = await this.credentialsService.getCredentials();
     return res.status(HttpStatus.OK).json(result);
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   providers: [UsersService, PrismaService],
   controllers: [UsersController ]
 })
